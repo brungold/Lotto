f40eb322fb63992257a4b086de26665d01510e86
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-10-10 01:28:29 +0200

    first commit

--- src/main/java/pl/lotto/domain/numberreceiver/NumberReceiverFacade.java
+++ src/main/java/pl/lotto/domain/numberreceiver/NumberReceiverFacade.java
@@ -1,4 +1,26 @@
 package pl.lotto.domain.numberreceiver;
 
+import java.util.List;
+import java.util.Set;
+
 public class NumberReceiverFacade {
+
+    public String inputNumbers(Set<Integer> numbersFromUser) {
+        List<Integer> filteredNumbers = filteredAllNumbersInRange(numbersFromUser);
+        if (areAllNumbersInRange(filteredNumbers)) {
+            return "success";
+        }
+        return "failed";
+    }
+
+    private static List<Integer> filteredAllNumbersInRange(Set<Integer> numbersFromUser) {
+        return numbersFromUser.stream()
+                .filter(number -> number >= 1)
+                .filter(number -> number <= 99)
+                .toList();
+    }
+
+    private boolean areAllNumbersInRange(List<Integer> filteredNumbers) {
+        return filteredNumbers.size() == 6;
+    }
 }
--- src/test/java/pl/lotto/domain/numberreceiver/NumberReceiverFacadeTest.java
+++ src/test/java/pl/lotto/domain/numberreceiver/NumberReceiverFacadeTest.java
@@ -1,7 +1,46 @@
 package pl.lotto.domain.numberreceiver;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class NumberReceiverFacadeTest {
 
+    @Test
+    public void should_return_success_when_user_gave_six_numbers() {
+        //given
+        NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade();
+        //when
+        String result = numberReceiverFacade.inputNumbers(Set.of(1, 2, 3, 4, 5, 6));
+        //then
+        assertThat(result).isEqualTo("success");
+    }
+
+    @Test
+    public void should_return_failed_when_user_gave_at_least_one_number_out_of_range_of_1_to_99() {
+        //given
+        NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade();
+        //when
+        String result = numberReceiverFacade.inputNumbers(Set.of(1, 2000, 3, 4, 5, 6));
+        //then
+        assertThat(result).isEqualTo("failed");
+    }
+
+    @Test
+    public void should_return_failed_when_user_gave_less_than_six_numbers() {
+        //given
+        NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade();
+        //when
+        String result = numberReceiverFacade.inputNumbers(Set.of(1, 2, 3, 4, 5));
+        //then
+        assertThat(result).isEqualTo("failed");
+    }
+
+//    @Test
+//    public void should_return_failed_when_user_gave_more_than_six_numbers() {
+//
+//    }
 }
