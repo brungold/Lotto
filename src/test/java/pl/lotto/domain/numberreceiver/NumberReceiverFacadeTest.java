ef7bf6cd0b06868d41df6f6abec53fddc2ab8b49
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-10-10 13:25:58 +0200

    separated some of the responsibilities of the NumberReceiverFacede class into a new  NumberValidator class. Created the InputNumberResultDto. Adjusted the test cases to the dto response. Added the lombock dependency

--- src/test/java/pl/lotto/domain/numberreceiver/NumberReceiverFacadeTest.java
+++ src/test/java/pl/lotto/domain/numberreceiver/NumberReceiverFacadeTest.java
@@ -1,6 +1,7 @@
 package pl.lotto.domain.numberreceiver;
 
 import org.junit.jupiter.api.Test;
+import pl.lotto.domain.numberreceiver.dto.InputNumberResultDto;
 
 import java.util.Set;
 
@@ -8,35 +9,38 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class NumberReceiverFacadeTest {
+    NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade(
+            new NumberValidator()
+    );
 
     @Test
     public void should_return_success_when_user_gave_six_numbers() {
         //given
-        NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade();
+        Set<Integer> numbersFromUser = Set.of(1, 2, 3, 4, 5, 6);
         //when
-        String result = numberReceiverFacade.inputNumbers(Set.of(1, 2, 3, 4, 5, 6));
+        InputNumberResultDto result = numberReceiverFacade.inputNumbers(numbersFromUser);
         //then
-        assertThat(result).isEqualTo("success");
+        assertThat(result.message()).isEqualTo("success");
     }
 
     @Test
     public void should_return_failed_when_user_gave_at_least_one_number_out_of_range_of_1_to_99() {
         //given
-        NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade();
+        Set<Integer> numbersFromUser = Set.of(1, 2000, 3, 4, 5, 6);
         //when
-        String result = numberReceiverFacade.inputNumbers(Set.of(1, 2000, 3, 4, 5, 6));
+        InputNumberResultDto result = numberReceiverFacade.inputNumbers(numbersFromUser);
         //then
-        assertThat(result).isEqualTo("failed");
+        assertThat(result.message()).isEqualTo("failed");
     }
 
     @Test
     public void should_return_failed_when_user_gave_less_than_six_numbers() {
         //given
-        NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade();
+        Set<Integer> numbersFromUser = Set.of(1, 2, 3, 4, 5);
         //when
-        String result = numberReceiverFacade.inputNumbers(Set.of(1, 2, 3, 4, 5));
+        InputNumberResultDto result = numberReceiverFacade.inputNumbers(numbersFromUser);
         //then
-        assertThat(result).isEqualTo("failed");
+        assertThat(result.message()).isEqualTo("failed");
     }
 
 //    @Test
